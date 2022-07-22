ce7dd3bdfddc4e6984b79c995fb8d2e4f21d29fb
Hans Zizold <hzizold@hotmail.com>
2022-07-22 10:58:48 -0400

    Update PersistentBugger.js

--- PersistentBugger.js
+++ PersistentBugger.js
@@ -1,7 +1,26 @@
 // Write a function that takes a positive number num and returns its multiplicative persistence, which is the number of steps it takes to multiply all the digits of num by each other, and repeating with the product until a single digit is obtained.
 
+const multiplydigits = num => {
+  let newnum=1;
+  while(num > 0) {
+    let digit = num % 10;
+    newnum *= digit;
+    num = (num - digit)/10;
+  }
+  return newnum;
+  };
+
 const persistence = num => {
-  
+  let result = Infinity, steps = 0;
+  if (num < 10) {
+    return 0;
+  }
+  while(result >= 10) {
+    result = multiplydigits(num);
+    num = result;
+    steps += 1;
+  }
+  return steps;
 }
 
 console.log(persistence(999)); // 4
